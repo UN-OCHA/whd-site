cae4c23559df0226b88c33b132bb2f2f2225a4ed
Lisa Corcoran <lcdemc@gmail.com>
2020-08-06 12:23:19 +0200

    WHD-6 theme (#21)
    * fix font path
    
    * template override for hero paragraph tye, and associated library with js using youtube api to load video and show/hide button and image on click
    
    * js using youtube api to load video and show/hide button and image on click
    
    * hero paragraph type config and translation
    
    * add classes to elements for styling
    
    * styles for hero image and play button
    
    * add onPlayerReady function and set css styles on wrapper and iframe to achieve responsive video, set additional playerVars
    
    * add localConfig.example.json to theme so we can copy and gulp tasks run

---
+++ html/themes/custom/whd/js/hero-video.js
@@ -0,0 +1,75 @@
+(function ($) {
+  'use strict';
+
+  Drupal.behaviors.heroVideo = {
+    attach: function (context, settings) {
+
+      function createVideo() {
+        var youtubeScriptId = 'youtube-api';
+        var youtubeScript = document.getElementById(youtubeScriptId);
+
+        if (youtubeScript === null) {
+          var tag = document.createElement('script');
+          var firstScriptTag = document.getElementsByTagName('script')[0];
+
+          tag.src = 'https://www.youtube.com/iframe_api';
+          tag.id = youtubeScriptId;
+          firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        }
+
+        window.onYouTubeIframeAPIReady = function () {
+          window.player = new window.YT.Player('player', {
+            width: '100%',
+            height: 'auto',
+            videoId: '8qfpnFZQWmo',
+            playerVars: {
+              autoplay: 1,
+              controls: 0,
+              enablejsapi: 1,
+              fs: 0,
+              modestbranding: 1,
+              playsinline: 1,
+              rel: 0,
+              widget_referrer: 'WHD 2020'
+            },
+            events: {
+              'onReady': onPlayerReady,
+              'onStateChange': onPlayerStateChange
+            }
+          });
+        };
+      }
+
+      $('#hero__play-button').on('click', function () {
+        console.log('play');
+        $(this).hide();
+        $('#hero__image').hide();
+        createVideo();
+      });
+
+      function onPlayerReady(event) {
+        $('#hero__video-container').css({
+          'position': 'relative',
+          'padding-bottom': '56.25%',
+          'height': '0'
+        });
+        $('#player').css({
+          'position': 'absolute',
+          'top': '0',
+          'left': '0',
+          'width': '100%',
+          'height': '100%'
+        });
+      }
+
+      function onPlayerStateChange(event) {
+        if (event.data === 0) {
+          $('#player').remove();
+          $('#hero__video-container').append('<div id ="player" width="100%" height="100%"></div>').css('padding-bottom', '0');
+          $('#hero__play-button').show();
+          $('#hero__image').show();
+        }
+      }
+    }
+  };
+})(jQuery);
