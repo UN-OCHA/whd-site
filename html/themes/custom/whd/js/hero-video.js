e039dfc89fba0f1c495d73a07a347e8c13e8ea8f
left23 <lcdemc@gmail.com>
2020-08-06 12:11:40 +0200

    add onPlayerReady function and set css styles on wrapper and iframe to achieve responsive video, set additional playerVars

--- html/themes/custom/whd/js/hero-video.js
+++ html/themes/custom/whd/js/hero-video.js
@@ -17,37 +17,59 @@
           firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
         }
 
-        window.onYouTubeIframeAPIReady = function() {
+        window.onYouTubeIframeAPIReady = function () {
           window.player = new window.YT.Player('player', {
+            width: '100%',
+            height: 'auto',
             videoId: '8qfpnFZQWmo',
             playerVars: {
               autoplay: 1,
+              controls: 0,
+              enablejsapi: 1,
+              fs: 0,
               modestbranding: 1,
-              rel: 0
+              playsinline: 1,
+              rel: 0,
+              widget_referrer: 'WHD 2020'
             },
             events: {
+              'onReady': onPlayerReady,
               'onStateChange': onPlayerStateChange
             }
           });
-        }
+        };
       }
 
       $('#hero__play-button').on('click', function () {
         console.log('play');
         $(this).hide();
-        $("#hero__image").hide();
+        $('#hero__image').hide();
         createVideo();
       });
 
-      function onPlayerStateChange(event) {
-        if (event.data === 0) {
-          $("#player").remove();
-          $("#hero__video-container").append('<div id ="player" width="300" align="left" height="238" style="margin-right:30px;"></div>');
-          $("#hero__play-button").show();
-          $("#hero__image").show();
-        }
+      function onPlayerReady(event) {
+        $('#hero__video-container').css({
+          'position': 'relative',
+          'padding-bottom': '56.25%',
+          'height': '0'
+        });
+        $('#player').css({
+          'position': 'absolute',
+          'top': '0',
+          'left': '0',
+          'width': '100%',
+          'height': '100%'
+        });
       }
 
+      function onPlayerStateChange(event) {
+        if (event.data === 0) {
+          $('#player').remove();
+          $('#hero__video-container').append('<div id ="player" width="100%" height="100%"></div>').css('padding-bottom', '0');
+          $('#hero__play-button').show();
+          $('#hero__image').show();
+        }
+      }
     }
   };
 })(jQuery);
